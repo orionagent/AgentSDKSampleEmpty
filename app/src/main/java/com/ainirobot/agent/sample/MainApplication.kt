86cab698f6fe06bb1950aa29256edfdafc3f1e18
Yanli <peiyanli@cmcm.com>
2025-05-22 14:43:22 +0800

    init

---
+++ app/src/main/java/com/ainirobot/agent/sample/MainApplication.kt
@@ -0,0 +1,11 @@
+package com.ainirobot.agent.sample
+
+import android.app.Application
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
